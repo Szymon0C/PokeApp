b372c14ddb8e4c0f0e2edc39e9dd8f17f55e84fe
Szymon0C <szymon.cichonski30@gmail.com>
2023-02-08 16:35:44 +0100

    add endpoints

--- be/actions/api/userActions.js
+++ be/actions/api/userActions.js
@@ -18,7 +18,7 @@ class UserActions {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    const favourite = req.body.password;
+    const favourite = req.body.favourite;
 
     try {
       user = new User({ name, email, password, favourite });
@@ -28,6 +28,42 @@ class UserActions {
     }
 
     res.status(201).json(user);
+  }
+
+  async updateUser(req, res) {
+    let user;
+    const id = req.params.id;
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const favourite = req.body.favourite;
+
+    try {
+      user = await User.findOne({ _id: id });
+      user.name = name;
+      user.email = email;
+      user.password = password;
+      user.favourite = favourite;
+
+      await user.save();
+    } catch (err) {
+      return res.status(422).json({ message: err.message });
+    }
+
+    res.status(201).json(user);
+  }
+
+  async getUser(req, res) {
+    const id = req.params.id;
+    let user;
+
+    try {
+      user = await User.findOne({ _id: id });
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+
+    res.status(200).json(user);
   }
 }
 
--- be/routes/api.js
+++ be/routes/api.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllUsers, saveUser } = require("../actions/api/userActions");
+const {
+  getAllUsers,
+  saveUser,
+  updateUser,
+  getUser,
+} = require("../actions/api/userActions");
 
 router.get("/users", getAllUsers);
+
+router.get("/users/:id", getUser);
+
 router.post("/users", saveUser);
+
+router.put("/users/:id", updateUser);
 
 module.exports = router;
